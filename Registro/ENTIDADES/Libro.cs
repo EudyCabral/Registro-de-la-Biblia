6f3cec714898be344e0c9c0545fa032db5f3edd7
Eudy Cabral Burgos <eudy_cabral@ucne.edu.do>
2018-05-20 17:01:45 -0400

    Registro biblia

---
+++ Registro/ENTIDADES/Libro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Registro.ENTIDADES
+{
+    public class Libro
+    {
+        [Key]
+
+        public int TipoId { get; set; }
+        public int Id { get; set; }
+        public string siglas { get; set; }
+        public string descripcion { get; set; }
+        
+        //Constructor lleno del objeto 
+        public Libro(int Tipoid,int id, string siglas, string descripcion)
+        {
+            TipoId = Tipoid;
+            Id = id;
+            this.siglas = siglas;
+            this.descripcion = descripcion;
+        }
+        //
+        //Constructor vacio del objeto
+        public Libro()
+        {
+            TipoId = 0;
+            Id = 0;
+            siglas = string.Empty;
+            descripcion = string.Empty;
+        }
+    }
+}
